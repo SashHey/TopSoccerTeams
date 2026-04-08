ed6acf6fd993cc875e8ed51999abb6103f9721a9
Sasha T Heyburgh <sasha.heyburgh@gmail.com>
2026-04-08 09:14:49 +0200

    assigned textview to variable and display array items

--- app/src/main/java/com/example/topsoccerteams/MainActivity.kt
+++ app/src/main/java/com/example/topsoccerteams/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.topsoccerteams
 
 import android.os.Bundle
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -16,6 +17,13 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+
+        //creating variable to hold textview tex
+        val teamsTxt = findViewById<TextView>(R.id.teamsDisplayId)
+
+        //displaying array item in textview
+        teamsTxt.text = teams[0]
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
